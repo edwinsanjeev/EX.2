18e70c9148cc1f0df52a339049b70abdb055721f
EdwinSanjeev <94860839+edwinsanjeev@users.noreply.github.com>
2021-11-23 17:32:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Programmer: Edwin
+# Date: 11/22/2021
+# Description: Exercise 02
+
+name = input("Enter some text:")
+
+print(name)
+print("SQUAWK")
+print(name)
+print("SQUAWK")
+print(name)
+print("SQUAWK")
+print(name)
+print("SQUAWK")
+print(name)
+print("SQUAWK")
